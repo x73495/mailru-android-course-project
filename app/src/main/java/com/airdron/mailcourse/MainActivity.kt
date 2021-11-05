383b38cd85f3e08a663b58fb10359cc3f3c630ae
Andrew Oparin <a.oparin@corp.mail.ru>
2021-11-06 01:14:29 +0300

    add colorable navigation

--- app/src/main/java/com/airdron/mailcourse/MainActivity.kt
+++ app/src/main/java/com/airdron/mailcourse/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.airdron.mailcourse
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        findViewById<TextView>(R.id.hello_world_text_view).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra("", "")
+            }
+
+            startActivity(intent)
+        }
     }
 }
