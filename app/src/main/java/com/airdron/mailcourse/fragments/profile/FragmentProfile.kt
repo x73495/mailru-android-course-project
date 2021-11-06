059daed33ad1b2ba02112e4f48e67099400b570d
Artyom Davydov <x43872@gmail.com>
2021-11-06 15:11:15 +0300

    Add placeholder navigation buttons and remove some useless code
    Removed all the automatically generated stuff that we don't need right now

--- app/src/main/java/com/airdron/mailcourse/fragments/profile/FragmentProfile.kt
+++ app/src/main/java/com/airdron/mailcourse/fragments/profile/FragmentProfile.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.airdron.mailcourse.R
 
 class FragmentProfile : Fragment() {
@@ -15,5 +17,17 @@ class FragmentProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<Button>(R.id.button_to_drug_list).setOnClickListener {
+            it.findNavController().navigate(R.id.action_fragmentProfile_to_fragmentDrugList)
+        }
+
+        view.findViewById<Button>(R.id.button_to_course_history).setOnClickListener {
+            it.findNavController().navigate(R.id.action_fragmentProfile_to_fragmentCourseHistory)
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/airdron/mailcourse/fragments/schedule/FragmentSchedule.kt
+++ app/src/main/java/com/airdron/mailcourse/fragments/schedule/FragmentSchedule.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.airdron.mailcourse.R
 
 class FragmentSchedule : Fragment() {
@@ -15,5 +17,17 @@ class FragmentSchedule : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_schedule, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<Button>(R.id.button_to_course_edit).setOnClickListener {
+            it.findNavController().navigate(R.id.action_fragmentSchedule_to_fragmentCourseEdit)
+        }
+
+        view.findViewById<Button>(R.id.button_to_course_add).setOnClickListener {
+            it.findNavController().navigate(R.id.action_fragmentSchedule_to_fragmentCourseAdd)
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 }
