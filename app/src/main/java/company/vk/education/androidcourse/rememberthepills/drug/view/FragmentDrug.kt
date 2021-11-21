6e19ca380c2920adaade655c78402f8565687e1a
Andrew Oparin <a.oparin@corp.mail.ru>
2021-11-21 18:14:13 +0300

    pass navigation args

--- app/src/main/java/company/vk/education/androidcourse/rememberthepills/drug/view/FragmentDrug.kt
+++ app/src/main/java/company/vk/education/androidcourse/rememberthepills/drug/view/FragmentDrug.kt
@@ -5,69 +5,83 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Button
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import company.vk.education.androidcourse.rememberthepills.R
-import company.vk.education.androidcourse.rememberthepills.components.storage.models.DrugTypeItem
+import company.vk.education.androidcourse.rememberthepills.components.adapter.form.BaseRecyclerViewAdapter
+import company.vk.education.androidcourse.rememberthepills.components.form.viewHolder.FormViewHolderFactory
+import company.vk.education.androidcourse.rememberthepills.databinding.FragmentDrugBinding
 import company.vk.education.androidcourse.rememberthepills.drug.viewModel.DrugViewModel
+import company.vk.education.androidcourse.rememberthepills.drug.viewModel.DrugViewModelFactory
 
 class FragmentDrug : Fragment() {
 
+    private var _binding: FragmentDrugBinding? = null
+    private val binding: FragmentDrugBinding get() = _binding!!
+
     val args: FragmentDrugArgs by navArgs()
-    val drugViewModel: DrugViewModel by viewModels()
+    val drugViewModel: DrugViewModel by viewModels() {
+        DrugViewModelFactory(args.mode, args.drugId)
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_drug, container, false)
+        val binding = FragmentDrugBinding.inflate(inflater, container, false)
+        _binding = binding
+
+        val viewHolderFactory = FormViewHolderFactory()
+        binding.drugFormRecyclerView.adapter = BaseRecyclerViewAdapter(viewHolderFactory)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        when (args.action) {
-            "add" -> {
-                view.findViewById<Button>(R.id.button_drug_remove).visibility = View.GONE
-                view.findViewById<Button>(R.id.button_drug_save).setText(R.string.add)
-            }
-            "edit" -> {
-                view.findViewById<AutoCompleteTextView>(R.id.input_text_drug_name)
-                    .setText("Препарат с ID ${args.idOfDrug}")
-                view.findViewById<AutoCompleteTextView>(R.id.input_text_drug_type)
-                    .setText("Тип препарата с ID ${args.idOfDrug}")
-
-                view.findViewById<Button>(R.id.button_drug_save).setText(R.string.save)
-            }
+        when (args.mode) {
+//            "add" -> {
+//                view.findViewById<Button>(R.id.button_drug_remove).visibility = View.GONE
+//                view.findViewById<Button>(R.id.button_drug_save).setText(R.string.add)
+//            }
+//            "edit" -> {
+//                view.findViewById<AutoCompleteTextView>(R.id.input_text_drug_name)
+//                    .setText("Препарат с ID ${args.idOfDrug}")
+//                view.findViewById<AutoCompleteTextView>(R.id.input_text_drug_type)
+//                    .setText("Тип препарата с ID ${args.idOfDrug}")
+//
+//                view.findViewById<Button>(R.id.button_drug_save).setText(R.string.save)
+//            }
         }
 
-        val itemsType = DrugTypeItem.values().map { getString(it.textId) }
-        val adapterItemsMeasurement =
-            ArrayAdapter(requireContext(), R.layout.item_text_view_drop_down_list, itemsType)
-        (view.findViewById<AutoCompleteTextView>(R.id.input_text_drug_type)).setAdapter(
-            adapterItemsMeasurement
-        )
+//        val itemsType = DrugTypeItem.values().map { getString(it.textId) }
+//        val adapterItemsMeasurement =
+//            ArrayAdapter(requireContext(), R.layout.item_text_view_drop_down_list, itemsType)
+//        (view.findViewById<AutoCompleteTextView>(R.id.input_text_drug_type)).setAdapter(
+//            adapterItemsMeasurement
+//        )
 
-        view.findViewById<Button>(R.id.button_drug_save).setOnClickListener {
-            it.findNavController().popBackStack()
-            // TODO: actual save
-        }
-
-        view.findViewById<Button>(R.id.button_drug_remove).setOnClickListener {
-            MaterialAlertDialogBuilder(requireContext())
-                .setTitle(getString(R.string.drug_removal_confirmation_title))
-                .setMessage(getString(R.string.drug_removal_confirmation_message))
-                .setNegativeButton(getString(R.string.removal_confirmation_answer_no)) { dialog, _ ->
-                    dialog.cancel()
-                }
-                .setPositiveButton(getString(R.string.removal_confirmation_answer_yes)) { _, _ ->
-                    // TODO: actual deletion
-                    it.findNavController().popBackStack()
-                }
-                .show()
-        }
+//        view.findViewById<Button>(R.id.button_drug_save).setOnClickListener {
+//            it.findNavController().popBackStack()
+//            // TODO: actual save
+//        }
+//
+//        view.findViewById<Button>(R.id.button_drug_remove).setOnClickListener {
+//            MaterialAlertDialogBuilder(requireContext())
+//                .setTitle(getString(R.string.drug_removal_confirmation_title))
+//                .setMessage(getString(R.string.drug_removal_confirmation_message))
+//                .setNegativeButton(getString(R.string.removal_confirmation_answer_no)) { dialog, _ ->
+//                    dialog.cancel()
+//                }
+//                .setPositiveButton(getString(R.string.removal_confirmation_answer_yes)) { _, _ ->
+//                    // TODO: actual deletion
+//                    it.findNavController().popBackStack()
+//                }
+//                .show()
+//        }
     }
 }
--- app/src/main/java/company/vk/education/androidcourse/rememberthepills/drug/viewModel/DrugViewModel.kt
+++ app/src/main/java/company/vk/education/androidcourse/rememberthepills/drug/viewModel/DrugViewModel.kt
@@ -1,6 +1,19 @@
 package company.vk.education.androidcourse.rememberthepills.drug.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import company.vk.education.androidcourse.rememberthepills.components.models.FormScreenMode
 
-class DrugViewModel: ViewModel() {
+class DrugViewModel(
+    private val mode: FormScreenMode,
+    private val id: Int?) : ViewModel() {
+}
+
+class DrugViewModelFactory(
+    private val mode: FormScreenMode,
+    private val id: Int?
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return DrugViewModel(mode, id) as T
+    }
 }
